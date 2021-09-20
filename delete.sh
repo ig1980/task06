ae26cbc249989a607500739958239db589fe8009
Igor Besedin <ibesedin@mail.ru>
2021-09-20 10:25:18 +0300

    added apply & delete scripts

---
+++ delete.sh
@@ -0,0 +1,16 @@
+kubectl delete -f client-ingress.yaml
+kubectl delete -f cilent-service.yaml
+kubectl delete -f client-deployment.yaml
+kubectl delete -f postgress-service.yaml
+kubectl delete -f postgress-deployment.yaml
+kubectl delete -f postgress-volume.yaml
+kubectl delete -f postgress-secret.yaml
+kubectl delete -f postgress-configmap.yaml
+
+
+
+
+
+
+
+
